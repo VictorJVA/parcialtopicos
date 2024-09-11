ec71b0adfb28d8777b7104ebede699d9b3261e4c
VictorJVA <130608466+VictorJVA@users.noreply.github.com>
2024-09-11 08:56:09 -0500

    Update urls.py

--- parcialtopicos/pages/urls.py
+++ parcialtopicos/pages/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, RegisterFlightView, ProductListFlightsView, FlightStatsView
+from .views import HomePageView, AboutPageView, RegisterFlightView, ListFlightsView, FlightStatsView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name='home'),
-    path("", AboutPageView.as_view(), name='about'),
-    path("", RegisterFlightView.as_view(), name='register'),
-    path("", ProductListFlightsView.as_view(), name='list'),
-    path("", FlightStatsView.as_view(), name='stats'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path("register/", RegisterFlightView.as_view(), name='register'),
+    path("list/", ListFlightsView.as_view(), name='list'),
+    path("stats/", FlightStatsView.as_view(), name='stats'),
 ]
 
 
